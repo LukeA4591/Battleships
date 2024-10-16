610300c669927df48c7d1020b7aa12b1d5620b09
lar93 <lar93@uclive.ac.nz>
2024-10-16 22:55:52 +1300

    forgot to add code

--- button.c
+++ button.c
@@ -5,13 +5,16 @@
 /** Return non-zero if button pressed.  */
 int button_pressed_p (void)
 {
-    /* TODO.  */
+    if ((PIND & (1<<7)) != 0) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 
 /** Initialise button1.  */
 void button_init (void)
 {
-    /* TODO.  */
+    DDRD &= ~(1<<7);
 }
-
